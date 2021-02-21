bbe27e5081a79efd5dda3463f30e408ace896ab1
Dwivedi Vishal Sridhar Arya <vishary@amazon.com>
2021-02-21 23:09:12 +0530

    Solution setup.

---
+++ Parallel_Paradigm/PP_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PP_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
