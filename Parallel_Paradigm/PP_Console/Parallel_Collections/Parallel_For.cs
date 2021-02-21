2b668e1ce51e19338cef1e94c5b1c0f461ef6e39
Dwivedi Vishal Sridhar Arya <vishary@amazon.com>
2021-02-21 23:20:01 +0530

    Create Code-Block Placeholders

---
+++ Parallel_Paradigm/PP_Console/Parallel_Collections/Parallel_For.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Console.Parallel_Collections
+{
+    public class Parallel_For
+    {
+        
+    }
+}
