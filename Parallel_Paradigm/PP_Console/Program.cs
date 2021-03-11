95139af47d4358eed2c61204683c41b263676aec
Dwivedi Vishal Sridhar Arya <vishary@amazon.com>
2021-03-11 11:38:44 +0530

    Adding Docs folder to parent.

--- Parallel_Paradigm/PP_Console/Program.cs
+++ Parallel_Paradigm/PP_Console/Program.cs
@@ -12,6 +12,10 @@ namespace PP_Console
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// Entery point of our program.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Runner();
@@ -20,12 +24,22 @@ namespace PP_Console
             Console.ReadKey();
         }
         /// <summary>
-        /// Staring point of the program, where all other pieces are called and
-        /// referred to.
+        /// Staring point of the program, where all other pieces are called and referred to.
+        /// 
+        /// In order to keep concepts / examples de-cluttered 
+        /// We have -
+        /// 
+        /// 1. Partitioned into different regions e.g [GENERIC - BASICS OF TASK PROGRAMMING] and so forth.
+        /// 2. Each section has its own separate callee point with its class and implementation.
+        /// 3. It's recommended, uncomment single callee point at a time and understand its complete code flow.
+        /// 
+        /// Example - Uncomment <code>//var g_canellation = new Cancellation_TPL();</code> and run,
+        /// to understand implementation of Cancellation tokens and thread operations.
         /// </summary>
         private static void Runner()
         {
-            // GENERIC - BASICS OF TASK PROGRAMMING
+
+            #region  GENERIC - BASICS OF TASK PROGRAMMING
             //var g_tpl = new Generic_TPL();
 
             // GENERIC - CANCELLATIONs IN TPL
@@ -34,9 +48,11 @@ namespace PP_Console
             // GENRIC - EXCEPTIONS HANDLING IN TPL
             //var g_exceptions = new Exceptions_TPL();
 
+            #endregion
 
             /*------------------------------------------------*/
 
+            #region SYNCHRONIZATION AND LOCKING
             // SYNCHRONIZATION AND LOCKING - USING CRITICAL SECTION
             //var sync_criticalSection = new CriticalSections();
 
@@ -49,13 +65,23 @@ namespace PP_Console
             // SYNCHRONIZATION AND LOCKING - SEMAPHORE
             //var sync_semaphore = new Semaphore();
 
+            #endregion
+
             /*------------------------------------------------*/
 
-            //
+            #region PARALLEL COLLECTION
+            // PARALLEL LOOP - FOR
             //var parallel_loop = new Parallel_For();
 
-            //
+            #endregion
+
+            /*------------------------------------------------*/
+
+            #region PUB-SUB BEHAVIOUR IN TASK
+            // PUBLISHER-CONSUMER RELATIONSHIP 
             var produce_consume = new Producer_Consumer();
+
+            #endregion
 
             Console.WriteLine("Back to Runner");
         }
